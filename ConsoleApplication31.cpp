c6fabcd24c41024e3044a675704551adf3b7ae75
theomarsama <omarkillusama@gmail.com>
2024-11-28 14:43:22 +0100

    Basic Calculator.cpp

--- ConsoleApplication31.cpp
+++ ConsoleApplication31.cpp
@@ -1,58 +1,41 @@
 #include <iostream>
-#include <sstream> 
-#include <string>  
-#include <iomanip> 
-
+#include <string>
 using namespace std;
 
 int main() {
-    string input; 
-    double num1, num2, result; 
+    string input;
+    double num1, num2, result;
     char operation;
     char continueChoice;
 
     do {
         cout << "Enter a mathematical expression (e.g., 8 / 9): ";
-        getline(cin, input); 
+        cin >> num1 >> operation >> num2;
 
-       
-        stringstream ss(input);
-        ss >> num1 >> operation >> num2;
-
-       
-        switch (operation) {
-        case '+':
+        if (operation == '+') {
             result = num1 + num2;
-            break;
-        case '-':
+        } else if (operation == '-') {
             result = num1 - num2;
-            break;
-        case '*':
+        } else if (operation == '*') {
             result = num1 * num2;
-            break;
-        case '/':
+        } else if (operation == '/') {
             if (num2 != 0) {
                 result = num1 / num2;
-            }
-            else {
+            } else {
                 cout << "Error: Division by zero is not allowed.\n";
-                continue; 
+                continue;
             }
-            break;
-        default:
+        } else {
             cout << "Invalid operation. Please use +, -, *, or /.\n";
             continue;
+        }
 
-        // Display the result
-        cout << fixed << setprecision(2); 
         cout << "Result: " << num1 << " " << operation << " " << num2 << " = " << result << endl;
 
-        // Ask if the user wants to continue
         cout << "Do you want to perform another calculation? (y/n): ";
         cin >> continueChoice;
-        cin.ignore(); 
 
-    } while (continueChoice == 'y' || continueChoice == 'Y'); 
+    } while (continueChoice == 'y' || continueChoice == 'Y');
 
     cout << "Thank you for using the calculator. Goodbye!\n";
     return 0;
